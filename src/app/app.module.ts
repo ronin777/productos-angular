0ccda6d1ab12376165cf5a096e1ee338739c1cf8
Fredy Roman Vazquez Geronimo <crazzy-rock@live.com.mx>
2018-11-15 18:33:43 -0600

    Vista Home Vista previa al vista general

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { HomeComponent } from './components/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/home.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'home',
+    templateUrl: '../view/home.html'
+  })
+  export class HomeComponent {
+    public titulo: String;
+
+    constructor(){
+        this.titulo = 'WeApp de Productos';
+    }
+
+    ngOnInit() {
+        console.log('Se ha iniciado el Componente Home');
+    }
+  }
+  
